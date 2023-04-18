915b976bf494cd3292b9cf37e99e401517318670
emirmalika <emirmalika0712@gmail.com>
2023-04-18 18:50:55 +0600

    realize some tableView Delegate methods

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -30,5 +30,17 @@ class ToDoListViewController: UITableViewController {
         
         return cell
     }
+    
+    //MARK: TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
