c07bdd321ebf31b996054b7abb02cb83764a5466
emirmalika <emirmalika0712@gmail.com>
2023-04-19 18:42:14 +0600

    create an addBtn with alertContoller

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let toDoItems = ["1", "2", "3"]
+    var toDoItems = ["1", "2", "3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,5 +42,26 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (alertAction) in
+            self.toDoItems.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true)
+    }
+    
 }
 
